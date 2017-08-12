26c8362fbfdabd8438b5a27632670e0c28ce1f4d
Miceli <Rafael.Miceli@btgpactual.com>
2017-08-11 21:21:25 -0300

    Adicionando testes unitários

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
+test('should_get_lower_price', () => {
+  let product = {lastPrice: 2};
+  let expectedProduct = {lastPrice: 2, lowerPrice: 2};
+  expect(App.updateLowerPricesHistory(product, 3)).toBe(expectedProduct)
 });
