f438dd0a486e7fb1771033ba9013b776a33c52a4
Miceli <Rafael.Miceli@btgpactual.com>
2017-08-14 21:40:15 -0300

    testes cobrindo cases com decimal

--- src/App.test.js
+++ src/App.test.js
@@ -34,5 +34,32 @@ describe('should get lower price', () => {
 
     expect(app.findLowerPricesHistory(product, 4)).toBe(expectedPrice)
   });
+
+  test('equals 2 when lastprice is 2 and inputing 4 and lowerPrice is 2', () => {
+    let product = {lastPrice: 2, lowerPrice: 2};
+    let expectedPrice = 2;
+
+    let app = new App(); 
+
+    expect(app.findLowerPricesHistory(product, 4)).toBe(expectedPrice)
+  });
+
+  test('equals 1.75 when lastprice is 2 and inputing 4 and lowerPrice is 1.75', () => {
+    let product = {lastPrice: 2, lowerPrice: 1.75};
+    let expectedPrice = 1.75;
+
+    let app = new App(); 
+
+    expect(app.findLowerPricesHistory(product, 4)).toBe(expectedPrice)
+  });
+
+  test('equals 1.75 when lastprice is 1.80 and inputing 1.75 and lowerPrice is 1.77', () => {
+    let product = {lastPrice: 1.80, lowerPrice: 1.77};
+    let expectedPrice = 1.75;
+
+    let app = new App(); 
+
+    expect(app.findLowerPricesHistory(product, 1.75)).toBe(expectedPrice)
+  });
 })
 
