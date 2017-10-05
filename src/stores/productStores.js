504982d66ec7332d0003edf5367b60d697c4b2bb
Rafael Miceli <rafael.miceli@hotmail.com>
2017-10-05 20:57:12 -0300

    Product com state default

---
+++ src/stores/productStores.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+
+const defaultState = {
+    productsResume: [{
+        name: "",
+        lastPrice: 0,
+        minPrice: 0,
+        maxPrice: 0
+    }]
+}
