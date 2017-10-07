b68c688560263058bb138b3342406e9be0b09596
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-10-07 12:54:47 -0300

    Adicionado react-redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import { store } from './stores/productStores';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     document.getElementById('root')
 );
 
