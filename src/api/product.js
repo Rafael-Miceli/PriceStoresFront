21efd326f9c82ce68511180c4435de186eeca4ad
Rafael Miceli <rafael.miceli@hotmail.com>
2017-10-11 01:47:24 -0300

    Buscando informações de produtos da API

--- src/api/product.js
+++ src/api/product.js
@@ -9,5 +9,19 @@ const defaultState = {
 
 export const getAllProductsResume = cb => {
     console.log("Buscando produtos na base")
-    setTimeout(cb(defaultState.productsResume), 500)
+    fetch('http://localhost:5000/api/product', {
+        method: 'GET',
+    })
+    .then(response => {
+        if(response.status !== 200)
+            console.log("Algo deu errado ", response)
+
+        response.json()
+        .then(json => {
+            console.log("json retornado ", json)
+            cb(json);
+        })
+    });
 }
+
+//https://developers.google.com/web/updates/2015/03/introduction-to-fetch
