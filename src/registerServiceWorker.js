099bad824ef155743b5b3c2217964ed5d77eaf3f
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-11-25 08:49:14 -0200

    ServiceWorker dando problema em prod

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,13 +19,10 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
-
-  if ('serviceWorker' in navigator) {
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
-
-      console.log("Origem diferente")
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
@@ -35,11 +32,10 @@ export default function register() {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (!isLocalhost) {        
+      if (!isLocalhost) {
         // Is not local host. Just register service worker
         registerValidSW(swUrl);
       } else {
-        console.log("SW É Localhost")
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
       }
@@ -81,7 +77,6 @@ function checkValidServiceWorker(swUrl) {
   fetch(swUrl)
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
-      console.log('sw response ', response)
       if (
         response.status === 404 ||
         response.headers.get('content-type').indexOf('javascript') === -1
@@ -94,12 +89,13 @@ function checkValidServiceWorker(swUrl) {
         });
       } else {
         // Service worker found. Proceed as normal.
-        console.log("Registrando SW")
         registerValidSW(swUrl);
       }
     })
     .catch(() => {
-      console.log('No internet connection found. App is running in offline mode.');
+      console.log(
+        'No internet connection found. App is running in offline mode.'
+      );
     });
 }
 
